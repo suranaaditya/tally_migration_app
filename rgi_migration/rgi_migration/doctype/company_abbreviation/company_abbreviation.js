37a9a79abc16a192a07450c1f27a3b75f0da337d
Aditya Surana <aditya.surana@jewonline.in>
2026-04-19 14:09:04 +0000

    feat: Batch 1 DocTypes (Company Abbreviation + Mapping Rule Alternate Pattern)

---
+++ rgi_migration/rgi_migration/doctype/company_abbreviation/company_abbreviation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Dux Digitech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Company Abbreviation", {
+// 	refresh(frm) {
+
+// 	},
+// });
